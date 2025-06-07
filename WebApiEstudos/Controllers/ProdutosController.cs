168cd4d79c222079b4be26cf35d5f60593161874
gmand <gmand@DESKTOP-2KO8PEB>
2025-06-07 15:39:47 -0400

    httpget

---
+++ WebApiEstudos/Controllers/ProdutosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiEstudos.Data;
+using WebApiEstudos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEstudos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDataContext _context;
+        public ProdutosController(AppDataContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+        // GET: api/Produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+    }
+}
